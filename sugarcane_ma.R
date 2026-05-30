c5c162628d9f43fb1463eea10b496ddf150cfdf2
KhanhNguyenVoodoo <duy-khanh.nguyen@voodoo.io>
2026-05-30 20:00:50 +0200

    Add obs only model

--- sugarcane_ma.R
+++ sugarcane_ma.R
@@ -10,10 +10,16 @@ dat <- dat[!is.na(dat$PCY_pct) & !is.na(dat$delta_T_C), ]
 dat$obs <- factor(seq_len(nrow(dat)))
 dat$vi  <- 0
 
+m_obs <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
+                 random = list(~ 1 | obs),
+                 data = dat, method = "REML")
+summary(m_obs)
+
 m_base <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
                  random = list(~ 1 | obs, ~ 1 | crop_model),
                  data = dat, method = "REML")
 summary(m_base)
+anova(m_obs, m_base)
 
 m_site <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
                  random = list(~ 1 | obs, ~ 1 | site, ~ 1 | crop_model),
@@ -21,22 +27,22 @@ m_site <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
 summary(m_site)
 anova(m_base, m_site)
 
-m_ext <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
+m_study <- rma.mv(yi = PCY_pct, V = vi, mods = ~ delta_T_C,
                 random = list(~ 1 | study_id/obs, ~ 1 | crop_model),
                 data = dat, method = "REML")
-summary(m_ext)
-anova(m_base, m_ext)
+summary(m_study)
+anova(m_base, m_study)
 
 data.frame(model  = c("obs + crop", "obs + site + crop", "study/obs + crop"),
-           AIC    = c(AIC(m_base), AIC(m_site), AIC(m_ext)),
-           BIC    = c(BIC(m_base), BIC(m_site), BIC(m_ext)),
-           logLik = c(logLik(m_base), logLik(m_site), logLik(m_ext)),
+           AIC    = c(AIC(m_base), AIC(m_site), AIC(m_study)),
+           BIC    = c(BIC(m_base), BIC(m_site), BIC(m_study)),
+           logLik = c(logLik(m_base), logLik(m_site), logLik(m_study)),
            row.names = NULL)
-confint(m_ext, sigma2 = 1)
+confint(m_study, sigma2 = 1)
 
 
 pdf("ma_regplot.pdf", width = 7, height = 6)
-regplot(m_ext, mod = "delta_T_C", psize = 1, xlab = "Temperature increase (°C)", ylab = "PCY (%)")
+regplot(m_study, mod = "delta_T_C", psize = 1, xlab = "Temperature increase (°C)", ylab = "PCY (%)")
 dev.off()
 
 studies <- sort(unique(dat$study_id))
@@ -49,6 +55,6 @@ for (s in studies) {
   ds <- dat[dat$study_id == s, ]
   if (length(unique(ds$delta_T_C)) >= 2) abline(lm(PCY_pct ~ delta_T_C, ds), col = cols[s], lwd = 1.5)
 }
-abline(a = coef(m_ext)[1], b = coef(m_ext)[2], col = "black", lwd = 3)
+abline(a = coef(m_study)[1], b = coef(m_study)[2], col = "black", lwd = 3)
 legend("bottomleft", legend = studies, col = cols, pch = 19, lwd = 1.5, cex = 0.7, ncol = 2, bg = "white")
 dev.off()
